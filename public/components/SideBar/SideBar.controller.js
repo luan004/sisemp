a09a4b3b04c24d5710de7cded3a3511bde92dcaf
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2025-10-14 20:10:43 -0300

    feat: sidebar

--- public/components/SideBar/SideBar.controller.js
+++ public/components/SideBar/SideBar.controller.js
@@ -1,5 +1,9 @@
 angular.module('App').component('sideBar', {
     templateUrl: "/components/SideBar/SideBar.template.html",
+    transclude: true,
+    bindings: {
+        content: '@'
+    },
     controller: function($location) {
         var $ctrl = this;
 
@@ -12,12 +16,25 @@ angular.module('App').component('sideBar', {
                 title: "Início",
                 url: "/",
                 icon: "fa fa-home"
+            },
+            {
+                title: "Teste",
+                url: "/teste",
+                icon: "fa fa-home"
             }
         ];
 
         $ctrl.helpers = {
             itemIsActive: (path) => {
                 return $location.path() === path
+            }
+        }
+
+        $ctrl.events = {
+            hide: () => {
+                if ($ctrl.states.isActive) {
+                    $ctrl.states.isActive = false
+                }
             }
         }
     }
