ad265ccab085f255cfa9754b7f284a7f3c72c9bc
Tayna <taynavicente2019@gmail.com>
2025-11-18 08:13:57 -0300

    feat: adiciona tela de recuperação de senha

--- public/views/Login/Login.controller.js
+++ public/views/Login/Login.controller.js
@@ -1,25 +1,49 @@
-angular.module('App').controller('LoginController', function ($location) {
+angular.module('App').controller('LoginController', function ($location, $timeout) {
 
     this.form = {
         username: '',
         password: ''
-    }
+    };
+
+    this.recoverEmail = '';
 
     this.states = {
-        isFailed: false
-    }
-    
-    this.submit = function () {
-        this.states.isFailed = false
+        isFailed: false,
+        recoverMode: false,
+        recoverSuccess: false
+    };
 
-        /**
-         * Falha usuário e senha incorretos
-         */
+    this.submit = function () {
+        this.states.isFailed = false;
+
+        // Falha usuário e senha incorretos
         if (this.form.username != 'admin' || this.form.password != 'admin') {
-            this.states.isFailed = true
+            this.states.isFailed = true;
             return;
         }
 
-        $location.url('/')
-    }
+        $location.url('/');
+    };
+
+    this.recover = function () {
+
+        if (!this.recoverEmail) {
+            alert("Informe um e-mail válido.");
+            return;
+        }
+
+        this.recoverEmail = '';
+        this.states.recoverMode = false;
+        this.states.recoverSuccess = true;
+
+        $timeout(() => {
+            this.states.recoverSuccess = false;
+        }, 5000);
+    };
+
+    this.backToLogin = function () {
+        this.states.recoverMode = false;
+        this.states.recoverSuccess = false;
+    };
+
 });
